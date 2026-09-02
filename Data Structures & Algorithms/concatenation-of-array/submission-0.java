3630155c6874bacdf38263eb580f8a52f37a0f2d
Devarasetti Yaswanth Kiran <144648966+ykwise@users.noreply.github.com>
2026-09-02 15:45:54 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[2*n];
+        for(int i=0; i<nums.length; i++){
+            res[i] = nums[i];
+        }
+        for(int i=0; i<nums.length; i++){
+            res[i+n] = nums[i];
+        }
+
+        return res;
+        
+    }
+}
